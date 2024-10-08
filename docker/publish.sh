ff4cc2cbcc9a89fdf7623716f2f363dd50cb6b2b
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2024-10-08 15:50:03 +0400

    Update SAM to V2.1 (#6)
    * added sam 2.1
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update config.json
    
    * Update Dockerfile
    
    ---------
    
    Co-authored-by: MaxTeselkin <tesmaxxxx@gmail.com>

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/segment-anything-2:1.0.4 . && \
-docker push supervisely/segment-anything-2:1.0.4
+docker build -t supervisely/segment-anything-2:1.0.6 . && \
+docker push supervisely/segment-anything-2:1.0.6
