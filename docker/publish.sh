8b304369973274333b9c42debe7daff8e5d602e1
MaxTeselkin <tesmaxxxx@gmail.com>
2025-06-03 15:21:04 +0000

    upodated docker image

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/segment-anything-2:1.0.13 . && \
-docker push supervisely/segment-anything-2:1.0.13
+docker build -t supervisely/segment-anything-2:1.0.15 . && \
+docker push supervisely/segment-anything-2:1.0.15
