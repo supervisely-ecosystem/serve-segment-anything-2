02d665fb6e3c080727e9a16ea6d13080b56ebfd0
vorozhkog <vorozhkog@icloud.com>
2025-12-06 04:05:08 +0000

    init

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/segment-anything-2:1.0.16 . && \
-docker push supervisely/segment-anything-2:1.0.16
+docker build -t supervisely/segment-anything-2:1.0.17 . && \
+docker push supervisely/segment-anything-2:1.0.17
