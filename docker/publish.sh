f2f355b5f76e039fe5f31df3342afd00fd2cd20a
cxnt <pavelbartsits@gmail.com>
2025-12-10 14:03:24 +0000

    Update Supervisely SDK version to v6.73.483

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/segment-anything-2:1.0.17 . && \
-docker push supervisely/segment-anything-2:1.0.17
+docker build -t supervisely/segment-anything-2:1.0.18 . && \
+docker push supervisely/segment-anything-2:1.0.18
